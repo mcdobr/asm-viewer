0aba52c8025e0e82a3fbb607e649c323fe980a80
mircea.dobreanu <mircea.dobreanu@gmail.com>
2017-12-28 13:43:23 +0200

    working on interleaved output

--- html/compile.php
+++ html/compile.php
@@ -1,33 +1,44 @@
 <?php
+	function addDefaultAdditionalFlags(&$additional, $compiler) {
+		/* Set to the intel syntax */
+		if ($compiler === "gcc" && preg_match('/^-masm=/', $additional) == 0) {
+			$additional = $additional . " -masm=intel ";
+		}
+		if (preg_match('/^-fno-asynchronous-unwind-tables/', $additional) == 0) {
+			$additional = $additional . " -fno-asynchronous-unwind-tables ";
+		}
+	}
+
 	error_reporting(E_ALL);
 	ini_set('display_errors', '1');
 
 	$c_path = '/tmp/temp.c';
-	$asm_path = '/tmp/temp.s';
+	$asm_path = '/tmp/temp.lst';
 
 	$in = json_decode(stripslashes(file_get_contents("php://input")), true);
 	
-	
 	/* Compile the file and return the asm code */
 	file_put_contents($c_path, $in['code']);
-
 	$compiler = $in['compiler'];
 	$additional = $in['additional'];
 
-	/* Set to the intel syntax */
-	if ($compiler === "gcc" && preg_match('/^-masm=/', $additional) == 0) {
-		$additional = $additional . " -masm=intel";
-	}
-	
+	addDefaultAdditionalFlags($additional, $compiler);
 
 	/* Create the command line string, validate it and execute it  */	
-	$cmdline_string = "$compiler $c_path -S -o $asm_path -fno-asynchronous-unwind-tables " . $additional;	
-	$cmdline_string = escapeshellcmd($cmdline_string);	
-	exec($cmdline_string, $exec_output, $ret_code);
+	/*if (!file_exists($asm_path)) {
+		touch($asm_path);
+		chmod($asm_path, 0666);
+	}*/
 
+	$cmdline_string = escapeshellcmd("$compiler $c_path $additional -Wa,-adhln -g");
+	$status = exec($cmdline_string, $exec_output, $ret_code);
+	echo $cmdline_string . PHP_EOL;
+
+	var_dump($status);
+	var_dump($exec_output);
+	var_dump($ret_code);
 	//header("Content-Type: application/json");
-	//var_dump($exec_output);
-	
+
 	if ($ret_code !== 0) {
 		echo "Compilation failed";
 	} else {
@@ -38,5 +49,5 @@
 		/* Delete the temporary files */
 		//unlink($asm_path);
 	}
-	//unlink($c_path);
+	unlink($c_path);
 ?>
